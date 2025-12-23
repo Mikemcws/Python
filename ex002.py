a34c7a30ceb99e26825e923c9c039b229ddf8a5c
Mcwsmike <caciqye@hotmail.com>
2025-12-23 08:56:35 -0300

    commit

---
+++ ex002.py
@@ -0,0 +1,4 @@
+dia = input('Qual o dia do nascimento? ')
+mes = input('Qual o mes do nascimento? ')
+ano = input('Qual o ano de nascimento? ')
+print(f'Você nasceu no dia {dia} de {mes} de {ano}.Estou correto?')
