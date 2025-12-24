c51e6bf2a0d6ed5ebc6d6cc85631ecde1db72027
Mike Willian <caciqye@hotmail.com>
2025-12-24 09:00:14 -0300

    Refactor currency converter logic and input prompt

--- conversor_v1.py
+++ conversor_v1.py
@@ -1,16 +1,15 @@
 import requests
 
-# 1. Busca a cotação real (como você já aprendeu)
 url = "https://economia.awesomeapi.com.br/last/USD-BRL"
 dados = requests.get(url).json()
 cotacao = float(dados['USDBRL']['bid'])
 
 print(f"--- DÓLAR AGORA: R$ {cotacao:.2f} ---")
 
-# 2. O seu critério de "grana"
+
 preco_limite = float(input("Até quanto você aceita pagar pelo Dólar? R$ "))
 
-# 3. A Lógica de Decisão (O coração do projeto)
+
 if cotacao <= preco_limite:
     print("\n✅ OPORTUNIDADE: O preço está abaixo do seu limite!")
     print(f"Compre agora! Você está economizando R$ {preco_limite - cotacao:.2f} por dólar.")
